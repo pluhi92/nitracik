91ef08f2b7d3bdd56f530dca6b191e45b468b121
pluhi92 <tomee1992@gmail.com>
2025-11-19 21:03:31 +0100

    backend preparation

--- booking-backend/server.js
+++ booking-backend/server.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
